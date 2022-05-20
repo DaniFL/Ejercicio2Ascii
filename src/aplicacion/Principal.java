7f24ead10d86d707ac1d1ec2d060e3a8c65ca289
Danii <danielfernandezlopez75@gmail.com>
2022-05-20 09:42:36 +0200

    desarrollo

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,25 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+import dominio.ASCIIArtGenerator;
+public class Principal {
+
+    public static void main(String[] args) throws Exception {
+
+            ASCIIArtGenerator artGen = new ASCIIArtGenerator();
+
+            System.out.println();
+            artGen.printTextArt("JESUS ES MARICON", ASCIIArtGenerator.ART_SIZE_MEDIUM);
+            System.out.println();
+
+            /*System.out.println();
+            artGen.printTextArt("Love is life!", ASCIIArtGenerator.ART_SIZE_SMALL, ASCIIArtGenerator.ASCIIArtFont.ART_FONT_MONO,"@");
+            System.out.println();*/
+
+        }
 }
+
+
+
+            
+
+   
